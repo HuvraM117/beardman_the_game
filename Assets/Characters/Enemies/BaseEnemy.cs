c596646ade5f6cd2c864996c0df9b9e94ed44f67
Steven Xie <stevengxie@gmail.com>
2018-09-25 01:20:14 -0400

    Added baseline work on how we'll probably approach monsters
    Signed-off-by: Steven Xie <stevengxie@gmail.com>

---
+++ Assets/Characters/Enemies/BaseEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+
+    public int Health { get; set; }
+    public float Damage { get; set; }
+    public float MoveSpeed { get; set; }
+
+    // The player's Game Object we wish to track
+    public GameObject playerObject;
+
+    public BaseEnemy()
+    {
+        // Empty Constructor
+    }
+
+    public BaseEnemy(int health, float damage, float moveSpeed)
+    {
+        Health = health;
+        Damage = damage;
+        MoveSpeed = moveSpeed;
+    }
+
+    // These methods are virtual and can be overriden in child classes
+    public virtual void Attack()
+    {
+
+    }
+
+    public virtual void MoveLeft()
+    {
+
+    }
+
+    public virtual void MoveRight()
+    {
+
+    }
+
+}
