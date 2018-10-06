a1befcac04d28ed561cbf5c8f67ccf46785e2610
Caitlin Campbell <cac226@case.edu>
2018-10-06 19:18:14 -0400

    Added Huvra's script
    Damagable is Huvra's script

--- Assets/EnemyScripts/Movement/ElectricRazorMovement.cs
+++ Assets/EnemyScripts/Movement/ElectricRazorMovement.cs
@@ -4,11 +4,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* 
+ * Will move horizontally back and forth (TODO: make not move around platforms)
+*/
 public class ElectricRazorMovement : MonoBehaviour {
+
+	private int movingRight; // 1 if moving right, -1 if moving left
+	public int speed;
 
 	// Use this for initialization
 	void Start () {
-		
+		movingRight = 1;
 	}
 	
 	// Update is called once per frame
