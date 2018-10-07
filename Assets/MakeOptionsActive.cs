8866e43f6798d4cec8a738f016fe0ba271ca1b93
tylerkohrt <tck21@case.edu>
2018-10-07 14:29:08 -0400

    This is all of the main menu assets

---
+++ Assets/MakeOptionsActive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeOptionsActive : MonoBehaviour {
+
+	public GameObject optionsMenu;
+	public GameObject mainMenu;
+
+	public void onClick(){
+		optionsMenu.SetActive (true);
+		mainMenu.SetActive (false);
+	}
+}
