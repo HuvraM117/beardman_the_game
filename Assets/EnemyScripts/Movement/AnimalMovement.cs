ef818539682facb0deb8591d7350e01a012820be
Caitlin Campbell <cac226@case.edu>
2018-10-09 09:17:49 -0400

    Added jumping and renamed script

--- Assets/EnemyScripts/Movement/AnimalMovement.cs
+++ Assets/EnemyScripts/Movement/AnimalMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 //movement for the shaved animals
 //these move left to right
+//TODO: Test jumping 
 
 public class AnimalMovement : MonoBehaviour {
 
@@ -14,32 +15,44 @@ public class AnimalMovement : MonoBehaviour {
 	public double jumpProbability; // determines % of time enemy will jump if at end of platform
 									// e.g. jumpProbability = 0 means it will never jump
 									// 		jumpProbability = .5 means it will jump half the time
+	private bool jumping; // boolean that is true if character is jumping, false otherwise
+
 	private Rigidbody2D m_rigidbody;
 
 	// Use this for initialization
 	void Start () {
 		m_rigidbody = GetComponent<Rigidbody2D>();
 		movingRight = 1;
+		jumping = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Moves animal
-		Vector2 vector = new Vector2(1f * movingRight, 0f) * speed;
-		m_rigidbody.AddForce(vector);
+		if(!jumping) {
+			Vector2 vector = new Vector2(1f * movingRight, 0f) * speed;
+			m_rigidbody.AddForce(vector);
+		}
 	}
 
 	void reachEndOfPlatform() {
-		double doIJump = Random.Range(0f, 1f);
-		if (doIJump < jumpProbability) {
-			StartCoroutine(jump());
-		} else {
-			movingRight = -movingRight;
+		if (!jumping) {
+			double doIJump = Random.Range(0f, 1f);
+			if (doIJump < jumpProbability) {
+				StartCoroutine(jump());
+			} else {
+				movingRight = -movingRight;
+			}
 		}
 	}
 
 	private IEnumerator jump() {
-		//TODO JUMP
+		jumping = true;
+		float amountToMove = speed * Time.deltaTime;
+		Vector2 movement = new Vector2(movingRight * amountToMove, amountToMove);
+		m_rigidbody.AddForce(movement, ForceMode.Force);
+
+		jumping = false;
 		return null;
 	}
 
