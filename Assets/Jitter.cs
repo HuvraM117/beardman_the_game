840cb48a4e2270a45b93fddcfe0652b2d7c19b71
Brennan McFarland <bfm21@case.edu>
2018-10-24 11:37:25 -0400

    the barber jitters now (doesn't affect hitboxes or anything)

---
+++ Assets/Jitter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jitter : MonoBehaviour {
+
+    private Vector3 jitterOffset;
+    [SerializeField] private float JITTERFACTOR = 1f;
+    [SerializeField] private int JITTERPERIOD = 4;
+    private int jitterIteration = 0; // counter for what point we are in the period
+
+	// Use this for initialization
+	void Start () {
+        UpdateOffset();
+	}
+
+    private void FixedUpdate()
+    {
+        if(jitterIteration++ == JITTERPERIOD)
+        {
+            jitterIteration = 0;
+            UpdateOffset();
+        }
+
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, jitterOffset, Vector3.Distance(transform.localPosition, jitterOffset));
+    }
+
+    private void UpdateOffset()
+    {
+        // subtract .5 to make half the values negative and multiply by jitter factor
+        jitterOffset = new Vector3((Random.value -.5f) * JITTERFACTOR, (Random.value - .5f) * JITTERFACTOR, (Random.value - .5f) * JITTERFACTOR);
+    }
+}
