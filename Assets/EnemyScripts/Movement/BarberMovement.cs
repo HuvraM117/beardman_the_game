91cabea38607372fe1482b02399d7b6cf69f071b
Brennan McFarland <bfm21@case.edu>
2018-10-24 12:49:43 -0400

    the barber can now swoop in an arc

--- Assets/EnemyScripts/Movement/BarberMovement.cs
+++ Assets/EnemyScripts/Movement/BarberMovement.cs
@@ -20,16 +20,25 @@ public class BarberMovement : MonoBehaviour {
             }
         }
     }
-    [SerializeField] private float MOVESPEED = .1f;
+    [SerializeField] private float MOVESPEED = .1f; // base movement speed
+    [SerializeField] private float SWOOPSPEEDMULTIPLIER = 3f; // how much faster the barber travels while swooping
+    private float currentMoveSpeed; // movement speed with modifiers such as swooping multiplier
+    [SerializeField] private float SWOOPAMPLITUDE = 2f;
+    private bool isSwooping = false;
+    private float initialHeight;
+    private float initialX; // for keeping track of where in a swoop we are
+    private const float WAYPOINTTOLERANCE = .001f; // tolerance for when we've reached the waypoint, to avoid getting stuck due to floating point inaccuracies
     private Vector3 faceLeft;
     private Vector3 faceRight;
 
     private void Start()
     {
+        currentMoveSpeed = MOVESPEED;
         faceLeft = Vector3.Scale(transform.localScale, new Vector3(1f, 1f, 1f));
         faceRight = Vector3.Scale(transform.localScale, new Vector3(-1f, 1f, 1f));
 
         Waypoint = transform.position;
+        initialHeight = transform.position.y;
 
         // TODO: for testing, when done remove the below: move towards the target
         Waypoint = new Vector3(16f, Waypoint.y, Waypoint.z);
@@ -38,16 +47,43 @@ public class BarberMovement : MonoBehaviour {
     public void MoveTo(Vector3 newWaypoint)
     {
         Waypoint = newWaypoint;
+        currentMoveSpeed = MOVESPEED;
+        isSwooping = false;
+    }
+
+    public void SwoopTo(Vector3 newWaypoint)
+    {
+        Waypoint = newWaypoint;
+        initialX = transform.position.x;
+        currentMoveSpeed = MOVESPEED * SWOOPSPEEDMULTIPLIER;
+        isSwooping = true;
+    }
+
+    public bool IsWaypointReached()
+    {
+        return Mathf.Abs(Waypoint.x - transform.position.x) < WAYPOINTTOLERANCE;
     }
 
     private void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, Waypoint, MOVESPEED);
+        transform.position = Vector3.MoveTowards(transform.position, Waypoint, currentMoveSpeed);
 
-        // TODO: for testing, when done remove the below: move back and forth
-        if(Waypoint.x == transform.position.x)
+        // swoop in an arc
+        if(isSwooping)
         {
-            Waypoint = new Vector3((Waypoint.x == 16f) ? 9.5f : 16f, Waypoint.y, Waypoint.z);
+            float currentHeight = initialHeight - SWOOPAMPLITUDE * Mathf.Abs(Mathf.Sin(Mathf.PI * Mathf.Abs((transform.position.x - initialX) / (Waypoint.x - initialX))));
+            transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+        }
+
+        Debug.Log("Waypoint: " + Waypoint.x + "Transform: " + transform.position.x);
+        // TODO: for testing, when done remove the below: move back and forth, swooping when the waypoint is reached
+        if(IsWaypointReached())
+        {
+            Debug.Log("Waypoint reached");
+            if(isSwooping)
+                MoveTo(new Vector3((Mathf.Abs(Waypoint.x - 16f) < WAYPOINTTOLERANCE) ? 9.5f : 16f, Waypoint.y, Waypoint.z));
+            else
+                SwoopTo(new Vector3((Mathf.Abs(Waypoint.x - 16f) < WAYPOINTTOLERANCE) ? 9.5f : 16f, Waypoint.y, Waypoint.z));
         }
     }
 }
