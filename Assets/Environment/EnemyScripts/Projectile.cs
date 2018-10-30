88bc76728d8f6e4e8728b06aea2c4ff29a2826c8
Steven Xie <stevengxie@gmail.com>
2018-10-30 09:03:48 -0400

    Additional work on the barber tool 'Straight Edge'
    Signed-off-by: Steven Xie <stevengxie@gmail.com>

---
+++ Assets/Environment/EnemyScripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public bool moveRight;
+    public int speed;
+
+    private Rigidbody2D rigidbody;
+    private int timeToSelfDestruct = 4;
+
+    public Projectile(bool moveRight)
+    {
+        this.moveRight = moveRight;
+    }
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Vector2 velocity;
+	    if (moveRight)
+	    {
+	        velocity = new Vector2(speed, 0);
+        }
+	    else
+	    {
+            velocity = new Vector2(-speed, 0);
+	    }
+
+        rigidbody = GetComponent<Rigidbody2D>();
+	    rigidbody.velocity = velocity;
+
+        Destroy(gameObject, timeToSelfDestruct);
+    }
+	
+
+    // Collision w/ player
+    // TODO: Figure whether this can just be handled with the other script
+}
---
+++ Assets/Environment/EnemyScripts/StraightEdgeThrowing.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightEdgeThrowing : MonoBehaviour
+{
+    public bool facingRight;
+    public BoxCollider collider;
+    private GameObject projectile;
+    
+	// Use this for initialization
+	void Start () {
+		
+        // set the projectile object
+
+	}
+	
+	// Normalized update interval
+	void FixedUpdate () {
+		
+
+
+	}
+
+    // Check if player is within current range
+    bool CheckPlayerPosition()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left);
+
+        if (hit.collider != null)
+        {
+            if (hit.GetType().Equals("Beard Man"))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 'Fires' a projectile: initializes it with direction to fly
+     */
+    void FireProjectile()
+    {
+        // TODO: Implement
+    }
+
+}
