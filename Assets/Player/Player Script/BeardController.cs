fea0361eaef3e54d21130ff0525701e43bc4524a
Tianlong Chen <txc253@case.edu>
2018-11-05 16:59:54 -0500

    conflict resolve

--- Assets/Player/Player Script/BeardController.cs
+++ Assets/Player/Player Script/BeardController.cs
@@ -68,17 +68,10 @@ public class BeardController : MonoBehaviour
     // assuming the target is in range, not range-limited
     private void GrappleBeard(Vector2 target)
     {
-<<<<<<< HEAD
 
-
-        beardAnimator.WhipBeard(target);
-        var dir = (Vector2) target- beardman.position;
-		beardman.velocity = new Vector2 (beardman.velocity.x, 0);;
-=======
         beardAnimator.WhipBeard(target);
         var dir = (Vector2) target- beardman.position;
 		    beardman.velocity = Vector2.zero;
->>>>>>> master
         
 		beardman.AddForce(new Vector2(dir.x*2f, 0) * grappleForce , ForceMode2D.Impulse);
 		beardman.AddForce(dir * grappleForce, ForceMode2D.Impulse);
