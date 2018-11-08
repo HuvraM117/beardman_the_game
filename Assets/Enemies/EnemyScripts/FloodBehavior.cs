a944c2f54c4a54550e80d6949750c4176e0a813c
Huvra Mehta <huvramehta@gmail.com>
2018-11-08 10:11:23 -0500

    Updates to flood mechanic

--- Assets/Enemies/EnemyScripts/FloodBehavior.cs
+++ Assets/Enemies/EnemyScripts/FloodBehavior.cs
@@ -16,8 +16,23 @@ public class FloodBehavior : MonoBehaviour {
 	private int enemyCounter; 
 	private int maxEnemyCounter = 10;
 
+	private bool toRespawn; //Stop & Start Checkers
+	private bool toSpawn; 
+
+	List<GameObject> enemies = new List<GameObject>();
+
 	// Use this for initialization
 	void Start () {
+
+		toRespawn = false; 
+		toSpawn = true; 
+
+		enemies.Add(Squirrel);
+		enemies.Add(Bird);
+		//enemies.Add(Bear);
+		enemies.Add(StraightEdge);
+		//enemies.Add(ElectricRazor);
+		//enemies.Add(HairSpray);
 
 	}
 
@@ -26,6 +41,7 @@ public class FloodBehavior : MonoBehaviour {
 
 		timer += Time.deltaTime;
 		if (enemyCounter < maxEnemyCounter){
+			toRespawn = true; 
 			Respawn(); 
 		}
 
@@ -33,49 +49,51 @@ public class FloodBehavior : MonoBehaviour {
 
 	void OnCollisionEnter2D (Collision2D other)
 	{
+		Debug.Log("START FLOOD");
 		if (other.gameObject == player) {
 			InvokeRepeating ("Spawn", spawnTime, spawnTime);
-			enemyCounter++; 
 
 			//note: edit Damaged to minus enemyCounter value
 		}
 	}
 
+	void OnCollisionExit2D (Collision2D other)
+	{
+		Debug.Log("STOP FLOOD");
+		if (other.gameObject == player) {
+			toRespawn = false; 
+			toSpawn = false;
+		}
+	}
+
 	void Spawn ()
 	{
-		// If the player has no health left...
-		if(playerHealth.Health < 0)
+		// If the player has no health left, if the max enemies have generated, or we have left the trigger...
+		if(playerHealth.Health < 0 || enemyCounter == maxEnemyCounter || toSpawn == false)
 		{
-			// ... exit the function.
+			// ...exit the function.
 			return;
 		}
 
 		// Find a random index between zero and one less than the number of spawn points.
-
 		int spawnPointIndex = UnityEngine.Random.Range (0, spawnPoints.Length);
 
 		// Create an instance of one of our randomly choosen enemy prefabs 
-
-		List<GameObject> enemies = new List<GameObject>();
-		enemies.Add(Squirrel);
-		enemies.Add(Bird);
-		//enemies.Add(Bear);
-		enemies.Add(StraightEdge);
-		//enemies.Add(ElectricRazor);
-		//enemies.Add(HairSpray);
-
 		int random = UnityEngine.Random.Range(0, 2);
 		GameObject choosen = enemies[random];
 
 		Instantiate (choosen, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+		enemyCounter++; 
 	}
 
 	void Respawn()
 	{
-
-		if (timer >= deathCounter) {
-			timer = 0;
-			Spawn(); 
+		if (toRespawn == true) {
+			if (timer >= deathCounter) {
+				timer = 0;
+				//call spawn to generate a new enemy
+				Spawn (); 
+			}
 		}
 
 	}
