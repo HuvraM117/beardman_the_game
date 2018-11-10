007478bc358d821cc228a4c46f425d957a4f10b9
Peter Gao <petergao1@gmail.com>
2018-11-10 16:04:20 -0500

    Health Consumable 11/10
    Basic consumable with placeholder animation and generic scripts

---
+++ Assets/Player/HealthConsumable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthConsumable : Consumable {
+
+    public int duration;
+
+    public int healthRestore;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        onConsume(collision.gameObject);
+    }
+
+    public  override void onConsume(GameObject consumable)
+    {
+        PlayerState playerstate = consumable.GetComponent<PlayerState>();
+        if (playerstate != null)
+        {
+            playerstate.TakeDamage(0 - healthRestore);
+        }
+        gameObject.GetComponent<Animator>().SetTrigger("Consumed");
+        //gameObject.SetActive(false);
+    }
+}
