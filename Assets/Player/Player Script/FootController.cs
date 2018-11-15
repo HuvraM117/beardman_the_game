8a66d156a1c6f2e0f7812548db2112c24bae1aa9
Steven Xie <stevengxie@gmail.com>
2018-11-15 10:09:43 -0500

    Change to FootController that works independent of Flood's collider. Fixes the 'infinite' jump issue.
    Signed-off-by: Steven Xie <stevengxie@gmail.com>

--- Assets/Player/Player Script/FootController.cs
+++ Assets/Player/Player Script/FootController.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FootController : MonoBehaviour {
+public class FootController : MonoBehaviour
+{
 
     private Vector3 raycastOrigin = new Vector2(0f, -.9f);
     private RaycastHit2D downHit, rightHit, leftHit;
     private GameObject movingPlatform = null; // the moving platform the player is on if any
 
-    public bool IsGrounded { get
+    private bool isCurrentlyGrounded;
+
+    public bool IsGrounded
+    {
+        get
         {
             // yes if triggered and EITHER raycast down is positive OR raycast to sides is negative
-            return triggeredTriggers > 0 && (downHit.collider != null || (rightHit.collider == null && leftHit.collider == null));
+            //TODO: Difficulties w/ flood collider make raycast method difficult
+            ////return (triggeredTriggers > 0) && (downHit.collider != null || (rightHit.collider == null && leftHit.collider == null));
+            return isCurrentlyGrounded;
         }
     }
 
@@ -21,20 +28,20 @@ public class FootController : MonoBehaviour {
     private void Start()
     {
         triggerCount = gameObject.GetComponents<Collider2D>().Length;
+
+        //Physics.IgnoreCollision(floodObject.GetComponent<BoxCollider2D>(), GetComponent<BoxCollider2D>());
     }
 
     // update hits only once every physics update
     private void FixedUpdate()
     {
-        //update raycasts
-        downHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.down, .5f);
-        rightHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.right, .5f);
-        leftHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.left, .5f);
+        // Checks a position below the player's current one, checks for collision 
+        isCurrentlyGrounded = Physics2D.OverlapCircle(gameObject.transform.position + new Vector3(0, -1.0f, 0), 0.2f, LayerMask.GetMask("GroundLayer"));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name != "Grapple Point")
+        if (collision.gameObject.name != "Grapple Point")
             triggeredTriggers++;
     }
 
