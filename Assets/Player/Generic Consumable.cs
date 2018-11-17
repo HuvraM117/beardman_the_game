a29cefe6cc0510f8bdf9c0e7ffc85fe8cd4fe14b
ddo3 <ddo3@case.edu>
2018-11-16 23:04:39 -0500

    added audio source to all scenes

---
+++ Assets/Player/Generic Consumable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericConsumable : Consumable{
+
+    public int duration;
+
+
+    public override void onConsume(GameObject consumable)
+    {
+
+    }
+
+    IEnumerator specificAction()
+    {
+        yield return new WaitForSeconds(duration);
+    }
+}
