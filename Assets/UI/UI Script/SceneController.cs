3500bf40cfb632d1f686442eabf2bc7df2179bd3
ddo3 <ddo3@case.edu>
2018-11-20 09:09:54 -0500

    added pause-game functionality

--- Assets/UI/UI Script/SceneController.cs
+++ Assets/UI/UI Script/SceneController.cs
@@ -49,6 +49,17 @@ public class SceneController : MonoBehaviour {
         
 	}
     
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1;
+    }
+
+
     public void GoToMenu()
     {
         SceneManager.LoadScene(0);
