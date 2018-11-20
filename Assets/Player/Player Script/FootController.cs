3897181f22417bb63f313a073c4c0a9f074e0c7d
Steven Xie <stevengxie@gmail.com>
2018-11-19 20:53:36 -0500

    Attempted fixes don't work so for in FootController
    Signed-off-by: Steven Xie <stevengxie@gmail.com>

--- Assets/Player/Player Script/FootController.cs
+++ Assets/Player/Player Script/FootController.cs
@@ -8,10 +8,24 @@ public class FootController : MonoBehaviour {
     private RaycastHit2D downHit, rightHit, leftHit;
     private GameObject movingPlatform = null; // the moving platform the player is on if any
 
+    private bool isFootGrounded;
+    private bool isLeftCollided;
+    private bool isRightCollided;
+
     public bool IsGrounded { get
         {
+            // The original version -- doesn't work within the bounds of the flood
             // yes if triggered and EITHER raycast down is positive OR raycast to sides is negative
-            return triggeredTriggers > 0 && (downHit.collider != null || (rightHit.collider == null && leftHit.collider == null));
+            //return triggeredTriggers > 0 && (downHit || (rightHit.collider == null && leftHit.collider == null));
+
+            return triggeredTriggers > 0 && (isFootGrounded || (rightHit.collider == null && leftHit.collider == null));
+            //return triggeredTriggers > 0 && (isFootGrounded || (isLeftCollided == false && isRightCollided == false));
+
+
+            // Theoretically all we need
+            //return isFootGrounded;
+
+            //return ((triggeredTriggers > 0) && (isFootGrounded);
         }
     }
 
@@ -27,9 +41,14 @@ public class FootController : MonoBehaviour {
     private void FixedUpdate()
     {
         //update raycasts
-        downHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.down, .5f);
-        rightHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.right, .5f);
-        leftHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.left, .5f);
+        downHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.down, .5f, LayerMask.GetMask("Ground"));
+        rightHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.right, .5f, LayerMask.GetMask("Ground"));
+        leftHit = Physics2D.Raycast(transform.position + raycastOrigin, Vector2.left, .5f, LayerMask.GetMask("Ground"));
+
+        isFootGrounded = Physics2D.OverlapCircle(gameObject.transform.position + new Vector3(0, -1.0f, 0), 0.2f, LayerMask.GetMask("Ground"));
+        isLeftCollided = Physics2D.OverlapCircle(gameObject.transform.position + new Vector3(-0.75f, 0, 0), 0.1f, LayerMask.GetMask("Ground"));
+        isRightCollided = Physics2D.OverlapCircle(gameObject.transform.position + new Vector3(0.75f, 0, 0), 0.1f, LayerMask.GetMask("Ground"));
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
