4cb8633bc27de5652479c2a24d7aa1b55c327b68
Caitlin Campbell <cac226@case.edu>
2018-11-29 09:12:37 -0500

    Merging with master

---
+++ Assets/Sound/SoundScripts/MainMenuVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuVolume : MonoBehaviour {
+
+    private AudioSource musicSource;
+
+    [SerializeField] private Slider musicBar;
+
+
+	// Use this for initialization
+	void Start () {
+
+        musicSource = GetComponent<AudioSource>();
+
+        musicBar.value = musicSource.volume;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        musicSource.volume = musicBar.value;
+    }
+}
