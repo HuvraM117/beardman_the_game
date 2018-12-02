19ab09e6908da3026ff5f071d5da874dd09d10a4
ddo3 <ddo3@case.edu>
2018-12-01 19:20:03 -0500

    started implementing new main menu

---
+++ Assets/UI/UI Script/HoverButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverButton : MonoBehaviour {
+
+    private Button b;
+    private Image image;
+
+
+    private string hoverColorString = "#41DB75D5";
+    private Color hoverColor;
+
+    Color initialImageColor;
+
+    // Use this for initialization
+    void Start () {
+
+        b = this.GetComponent<Button>();
+
+        image = this.GetComponent<Image>();
+
+        ColorUtility.TryParseHtmlString(hoverColorString, out hoverColor);
+
+        initialImageColor = image.color;
+
+        hoverColor.a = 1f;
+    }
+
+
+    public void enterHover()
+    {
+        image.color = hoverColor;
+
+    }
+
+    public void exitHover()
+    {
+        image.color = initialImageColor;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
