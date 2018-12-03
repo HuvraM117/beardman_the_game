1cf41d9eebe98ba4594ccdb8c1fb8ffd6348d19b
ddo3 <ddo3@case.edu>
2018-12-02 20:24:28 -0500

    added new music and pop up start menu

---
+++ Assets/UI/UI Script/StartPopUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPopUp : MonoBehaviour {
+
+    private SceneController sc;
+
+	// Use this for initialization
+	void Start () {
+
+        sc = new SceneController();
+        sc.PauseGame();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
