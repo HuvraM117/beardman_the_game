da6b4b8e88bdec5c2c3b63e609c8463f52a2d288
Tianlong Chen <txc253@case.edu>
2018-12-04 22:05:27 -0500

    added trampoline and grapple bar animation

---
+++ Assets/Environment/Tramp_Behavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tramp_Behavior : MonoBehaviour {
+	public Animator ani;
+
+	// Use this for initialization
+	void Start () {
+		ani=gameObject.GetComponent<Animator>();
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter2D(Collision2D col){
+
+		ani.SetBool ("On",true);
+		if (col.gameObject.name == "Beard Man"){
+
+
+
+		}
+
+	}
+	void OnCollisionExit2D(Collision2D col){
+		ani.SetBool ("On",false);
+
+		if (col.gameObject.name == "Beard Man"){
+
+
+			//ani.SetBool ("On",false);
+
+		}
+
+	}
+}
